fd717d29df96fca09a89a4e2caea40f42f95c4d1
Adidem23 <adityasuryawanshi5451@gmail.com>
2024-02-04 01:08:17 +0530

    Added Navbar and Hersosection

--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from '../ToBeUsed/NavBar';
+import HeroSection from '../ToBeUsed/HeroSection';
 
 const Landing = () => {
 
   return (
     <div>
-        <NavBar/>
+        <NavBar />
+        <HeroSection />
     </div>
   )
 }
